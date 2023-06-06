16054dafa76b9c3047471888549721be5186769c
FYHNJ <feiyuhui0822@gmail.com>
2023-06-06 23:58:15 +0200

    Added login logic

--- lib/Loginpage.dart
+++ lib/Loginpage.dart
@@ -11,6 +11,55 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMixin {
+  
+  late TextEditingController _usernameController;
+  late TextEditingController _passwordController;
+  String? _errorMessage;
+
+  @override
+  void initState() {
+    super.initState();
+    _usernameController = TextEditingController();
+    _passwordController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  void _login() {
+    String username = _usernameController.text;
+    String password = _passwordController.text;
+
+    List<String> errors = [];
+
+    if (username.isEmpty) {
+      errors.add('please enter user name');
+    }
+
+    if (password.isEmpty) {
+      errors.add('please enter password');
+    }
+
+    if (username != 'forfaio' || password != 'forfaio') {
+      errors.add('wrong user name or password');
+    }
+
+    if (errors.isNotEmpty) {
+      setState(() {
+        _errorMessage = errors.join(',\n ');
+      });
+      return;
+    }
+
+    // 登录成功
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>const MyHomePage(title: 'Forfait')));
+  }
+
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,13 +86,15 @@ class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMix
                 ),
               ),
               const SizedBox(height: 40),
-              const TextField(
-                decoration: InputDecoration(
+              TextField(
+                 controller: _usernameController,
+                decoration: const InputDecoration(
                   labelText: 'Username',
                 ),
               ),
-              const TextField(
-                decoration: InputDecoration(
+              TextField(
+                controller: _passwordController,
+                decoration: const InputDecoration(
                   labelText: 'Password',
                 ),
               ),
@@ -53,15 +104,18 @@ class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMix
                   Text('Mot de passe oublie'),  // Your text
                 ],
               ),
+              const SizedBox(height: 20),
+               if (_errorMessage != null) 
+                Text(
+                  'Error: $_errorMessage',
+                  style: const TextStyle(color: Colors.red),
+                ),
               Transform.translate(  // Added Transform.translate
                 offset: const Offset(0, 50),  // Downwards by 50 units
                 child: Transform.scale(  // Added Transform.scale
                   scale: 1.5,  // Scale by 1.5 times
                   child: ElevatedButton(
-                    onPressed: () {
-                      // Perform login action
-                      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>const MyHomePage(title: 'Forfait')));
-                    },
+                    onPressed: _login,
                     child: const Text('Connexion'),
                   ),
                 ),
