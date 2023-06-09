4d6a69569d8fe051232e53415201be7688c93dcd
XiyuLUO <christine.xiyu.luo@gmqil.com>
2023-06-09 13:18:15 +0200

    add 2 button

--- lib/Profile/profile.dart
+++ lib/Profile/profile.dart
@@ -14,17 +14,62 @@ class _Profile extends State<Profile> {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        Image.asset(
-          'assets/images/MarcDupont.jpg',
-          width: 80,
-          height: 80,
+        Container(
+          margin: const EdgeInsets.only(top: 32),
+          child: Image.asset(
+            'assets/images/MarcDupont.jpg',
+            width: 80,
+            height: 80,
+          ),
         ),
+        Container(
+          margin: const EdgeInsets.only(top: 20),
+          child: const Text(
+            'Marc Dupont',
+            style: TextStyle(
+              fontSize: 34,
+              color: Color.fromRGBO(0, 40, 85, 1),
+            ),
+          ),
+        ),
+        const SizedBox(height: 20), // 添加垂直间距
         ElevatedButton(
-          onPressed: () {
-            Navigator.pushReplacement(context,
-                MaterialPageRoute(builder: (context) => const LoginPage()));
-          },
-          child: const Text('Logout'),
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            backgroundColor: Colors.white, // 设置按钮背景色为白色
+            foregroundColor: const Color.fromRGBO(0, 40, 85, 1), // 设置按钮文字颜色
+            textStyle: const TextStyle(
+              fontSize: 14, // 设置按钮文字大小为20
+            ),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(25), // 设置按钮圆角
+              side: const BorderSide(
+                  color: Color.fromRGBO(0, 40, 85, 1)), // 设置按钮边框样式
+            ),
+          ),
+          child: const Text('Modifier mon profile',
+              style:
+                  TextStyle(color: Color.fromRGBO(0, 40, 85, 1))), // 设置按钮文字内容
+        ),
+        const SizedBox(height: 380),
+        ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            backgroundColor: const Color.fromRGBO(0, 40, 85, 1), // 设置按钮背景色
+            foregroundColor: Colors.white, // 设置按钮文字颜色
+            textStyle: const TextStyle(
+              fontSize: 28, // 设置按钮文字大小
+            ),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(25), // 设置按钮圆角
+              side: const BorderSide(
+                  color: Color.fromRGBO(0, 40, 85, 1)), // 设置按钮边框样式
+            ),
+          ),
+          child: const Text('Régler mes factures',
+              style: TextStyle(color: Colors.white)), // 设置按钮文字内容
         ),
       ],
     );
