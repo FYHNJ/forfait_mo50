802cebfe208b244fe244e9611ecc7886629e8486
XiyuLUO <christine.xiyu.luo@gmqil.com>
2023-06-09 22:46:39 +0200

    user profile page: return to accueil

--- lib/Profile/profile.dart
+++ lib/Profile/profile.dart
@@ -15,254 +15,262 @@ class Profile extends StatefulWidget {
 class _Profile extends State<Profile> {
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Positioned(
-          top: 32,
-          left: 0,
-          right: 0,
-          child: Align(
-            alignment: Alignment.center,
-            child: Image.asset(
-              'assets/images/${widget.user.userName}.jpeg',
-              width: 80,
-              height: 80,
-            ),
-          ),
-        ),
-        Align(
-          alignment: Alignment.topCenter,
-          child: Padding(
-            padding:
-                EdgeInsets.only(top: MediaQuery.of(context).padding.top + 131),
-            child: Text(
-              widget.user.userName,
-              semanticsLabel: widget.user.userFname,
-              style: const TextStyle(
-                fontSize: 34,
-                color: Color.fromRGBO(0, 40, 85, 1),
-              ),
-            ),
-          ),
-        ),
-        Align(
-          alignment: Alignment.topCenter,
-          child: Padding(
-            padding:
-                EdgeInsets.only(top: MediaQuery.of(context).padding.top + 206),
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const UserProfilePage(),
-                  ),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                backgroundColor: Colors.white, // 设置按钮背景色
-                foregroundColor: const Color.fromRGBO(0, 40, 85, 1), // 设置按钮文字颜色
-                textStyle: const TextStyle(
-                  fontSize: 14, // 设置按钮文字大小
-                ),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(25), // 设置按钮圆角
-                  side: const BorderSide(
-                      color: Color.fromRGBO(0, 40, 85, 1)), // 设置按钮边框样式
+    return Scaffold(
+      body: Container(
+        color: Colors.white, // 设置背景色为白色
+        child: Stack(
+          children: [
+            Positioned(
+              top: 32,
+              left: 0,
+              right: 0,
+              child: Align(
+                alignment: Alignment.center,
+                child: Image.asset(
+                  'assets/images/${widget.user.userName}.jpeg',
+                  width: 80,
+                  height: 80,
                 ),
               ),
-              child: const Text('Modifier mon profile',
-                  style: TextStyle(
-                      color: Color.fromRGBO(0, 40, 85, 1))), // 设置按钮文字内容
             ),
-          ),
-        ),
-        Align(
-          alignment: Alignment.topCenter,
-          child: Padding(
-            padding:
-                EdgeInsets.only(top: MediaQuery.of(context).padding.top + 354),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Container(
-                  height: 56,
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.black,
-                      width: 1,
-                    ),
-                  ),
-                  child: Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 24.0), // 设置左边距
-                        child: Image.asset(
-                          'assets/images/facture.jpeg',
-                          width: 24,
-                          height: 24,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 54.0), // 设置左边距
-                        child: TextButton(
-                          onPressed: () {},
-                          child: const Text(
-                            'Voir mes factures',
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: Colors.black,
-                            ),
-                          ),
-                        ),
-                      ),
-                      const Spacer(), // 占据剩余空间
-                      // 第三个元素，与屏幕右边缘的距离为 24
-                      Padding(
-                        padding: const EdgeInsets.only(right: 24),
-                        child: Image.asset(
-                          'assets/images/arrow.jpeg',
-                          width: 24,
-                          height: 24,
-                        ),
-                      ),
-                    ],
+            Align(
+              alignment: Alignment.topCenter,
+              child: Padding(
+                padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).padding.top + 131),
+                child: Text(
+                  widget.user.userName,
+                  semanticsLabel: widget.user.userFname,
+                  style: const TextStyle(
+                    fontSize: 34,
+                    color: Color.fromRGBO(0, 40, 85, 1),
                   ),
                 ),
-                Container(
-                  height: 56,
-                  decoration: const BoxDecoration(
-                    border: Border(
-                      left: BorderSide(
-                        color: Colors.black,
-                        width: 1,
+              ),
+            ),
+            Align(
+              alignment: Alignment.topCenter,
+              child: Padding(
+                padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).padding.top + 206),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            UserProfilePage(user: widget.user),
                       ),
-                      right: BorderSide(
-                        color: Colors.black,
-                        width: 1,
-                      ),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    backgroundColor: Colors.white, // 设置按钮背景色
+                    foregroundColor:
+                        const Color.fromRGBO(0, 40, 85, 1), // 设置按钮文字颜色
+                    textStyle: const TextStyle(
+                      fontSize: 14, // 设置按钮文字大小
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25), // 设置按钮圆角
+                      side: const BorderSide(
+                          color: Color.fromRGBO(0, 40, 85, 1)), // 设置按钮边框样式
                     ),
                   ),
-                  child: Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 24.0), // 设置左边距
-                        child: Image.asset(
-                          'assets/images/1.jpeg',
-                          width: 24,
-                          height: 24,
+                  child: const Text('Modifier mon profile',
+                      style: TextStyle(
+                          color: Color.fromRGBO(0, 40, 85, 1))), // 设置按钮文字内容
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.topCenter,
+              child: Padding(
+                padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).padding.top + 354),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Container(
+                      height: 56,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 1,
                         ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 54.0), // 设置左边距
-                        child: TextButton(
-                          onPressed: () {},
-                          child: const Text(
-                            'Mon forfait',
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: Colors.black,
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(left: 24.0), // 设置左边距
+                            child: Image.asset(
+                              'assets/images/facture.jpeg',
+                              width: 24,
+                              height: 24,
                             ),
                           ),
-                        ),
-                      ),
-                      const Spacer(), // 占据剩余空间
-                      // 第三个元素，与屏幕右边缘的距离为 24
-                      Padding(
-                        padding: const EdgeInsets.only(right: 24),
-                        child: Image.asset(
-                          'assets/images/arrow.jpeg',
-                          width: 24,
-                          height: 24,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  height: 56,
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.black,
-                      width: 1,
-                    ),
-                  ),
-                  child: Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 24.0), // 设置左边距
-                        child: Image.asset(
-                          'assets/images/logout.jpeg',
-                          width: 24,
-                          height: 24,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 54.0), // 设置左边距
-                        child: TextButton(
-                          onPressed: () {
-                            Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const LoginPage(),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 54.0), // 设置左边距
+                            child: TextButton(
+                              onPressed: () {},
+                              child: const Text(
+                                'Voir mes factures',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.black,
+                                ),
                               ),
-                            );
-                          },
-                          child: const Text(
-                            'Déconnexion',
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: Colors.black,
                             ),
+                          ),
+                          const Spacer(), // 占据剩余空间
+                          // 第三个元素，与屏幕右边缘的距离为 24
+                          Padding(
+                            padding: const EdgeInsets.only(right: 24),
+                            child: Image.asset(
+                              'assets/images/arrow.jpeg',
+                              width: 24,
+                              height: 24,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: 56,
+                      decoration: const BoxDecoration(
+                        border: Border(
+                          left: BorderSide(
+                            color: Colors.black,
+                            width: 1,
+                          ),
+                          right: BorderSide(
+                            color: Colors.black,
+                            width: 1,
                           ),
                         ),
                       ),
-                      const Spacer(), // 占据剩余空间
-                      // 第三个元素，与屏幕右边缘的距离为 24
-                      Padding(
-                        padding: const EdgeInsets.only(right: 24),
-                        child: Image.asset(
-                          'assets/images/arrow.jpeg',
-                          width: 24,
-                          height: 24,
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(left: 24.0), // 设置左边距
+                            child: Image.asset(
+                              'assets/images/1.jpeg',
+                              width: 24,
+                              height: 24,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 54.0), // 设置左边距
+                            child: TextButton(
+                              onPressed: () {},
+                              child: const Text(
+                                'Mon forfait',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                          ),
+                          const Spacer(), // 占据剩余空间
+                          // 第三个元素，与屏幕右边缘的距离为 24
+                          Padding(
+                            padding: const EdgeInsets.only(right: 24),
+                            child: Image.asset(
+                              'assets/images/arrow.jpeg',
+                              width: 24,
+                              height: 24,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: 56,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 1,
                         ),
                       ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-        Align(
-          alignment: Alignment.topCenter,
-          child: Padding(
-            padding:
-                EdgeInsets.only(top: MediaQuery.of(context).padding.top + 621),
-            child: ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                backgroundColor: const Color.fromRGBO(0, 40, 85, 1), // 设置按钮背景色
-                foregroundColor: Colors.white, // 设置按钮文字颜色
-                textStyle: const TextStyle(
-                  fontSize: 26, // 设置按钮文字大小
-                ),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(25), // 设置按钮圆角
-                  side: const BorderSide(
-                      color: Color.fromRGBO(0, 40, 85, 1)), // 设置按钮边框样式
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(left: 24.0), // 设置左边距
+                            child: Image.asset(
+                              'assets/images/logout.jpeg',
+                              width: 24,
+                              height: 24,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 54.0), // 设置左边距
+                            child: TextButton(
+                              onPressed: () {
+                                Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => const LoginPage(),
+                                  ),
+                                );
+                              },
+                              child: const Text(
+                                'Déconnexion',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                          ),
+                          const Spacer(), // 占据剩余空间
+                          // 第三个元素，与屏幕右边缘的距离为 24
+                          Padding(
+                            padding: const EdgeInsets.only(right: 24),
+                            child: Image.asset(
+                              'assets/images/arrow.jpeg',
+                              width: 24,
+                              height: 24,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              child: const Text('Régler mes factures',
-                  style: TextStyle(color: Colors.white)), // 设置按钮文字内容
             ),
-          ),
+            Align(
+              alignment: Alignment.topCenter,
+              child: Padding(
+                padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).padding.top + 621),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    backgroundColor:
+                        const Color.fromRGBO(0, 40, 85, 1), // 设置按钮背景色
+                    foregroundColor: Colors.white, // 设置按钮文字颜色
+                    textStyle: const TextStyle(
+                      fontSize: 26, // 设置按钮文字大小
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25), // 设置按钮圆角
+                      side: const BorderSide(
+                          color: Color.fromRGBO(0, 40, 85, 1)), // 设置按钮边框样式
+                    ),
+                  ),
+                  child: const Text('Régler mes factures',
+                      style: TextStyle(color: Colors.white)), // 设置按钮文字内容
+                ),
+              ),
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
--- lib/Profile/user_profile.dart
+++ lib/Profile/user_profile.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:user_library/user_library.dart';
+import 'package:forfait_mo50/main.dart';
 
 class UserProfilePage extends StatefulWidget {
-  const UserProfilePage({Key? key}) : super(key: key);
+  final User user;
+  const UserProfilePage({Key? key, required this.user}) : super(key: key);
 
   @override
   // ignore: library_private_types_in_public_api
@@ -13,7 +16,29 @@ class _UserProfilePageState extends State<UserProfilePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Edit User Profile'),
+        title: const Text(
+          'Editer mon profile',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 14,
+          ),
+        ),
+        leading: IconButton(
+          icon: Image.asset(
+            'assets/images/return.jpeg',
+            width: 24,
+            height: 24,
+          ), // 设置图片
+          onPressed: () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) =>
+                    MyHomePage(title: 'Forfait', user: widget.user),
+              ),
+            );
+          },
+        ),
       ),
       body: Container(
           // 编辑用户个人信息的界面内容
